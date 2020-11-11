0211ec91b1bf0c20cb9062aa2b449621b5b46fc4
jennyziyi-xu <56279639+jennyziyi-xu@users.noreply.github.com>
2020-11-11 09:04:13 -0500

    Finished student manager class, need to see other classes to write test code

--- hypatia/hypatia/settings.py
+++ hypatia/hypatia/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'studentsummary.apps.StudentsummaryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- hypatia/studentsummary/models.py
+++ hypatia/studentsummary/models.py
@@ -1,20 +1,30 @@
 from django.db import models
 
+class Student(models.Model):       # for purpose of testing only 
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    name = models.TextField()
+    topic = models. CharField(max_length=  100)
+    def __str__(self):
+        return self.name 
+
 class StudentManager(models.Model):
     question_bank = models.ManyToManyField(Question)
     students = models.ManyToManyField(Student)
 
     def __str__(self):
-        return self.students, self.questions
+        return self.students, self.question_bank
     
     def check_user(self, Student) -> bool:
         """
         Return whether the student is in the class. 
         """
         x = self.students.filter(name = Student.name)
-        if len(x) != 0:
-            return True
-        return False
+        return x.exists()
     
     def get_all_questions(self):
         return self.question_bank
@@ -26,7 +36,17 @@ class StudentManager(models.Model):
         x = self.questions.filer(topic = Topic)
         return x
     
-    def append_question(self,Question):
-        
+   def append_question(self,Question):
+       """
+       Add a question from question bank
+       """
+       self.question_bank.append(Question)
 
-
+    def delete_question(self,Question):
+        """
+        Delete a question frm question bank
+        """
+        return self.question_bank.exclude(prompt = Question.prompt)
+    
+    def get_all_students(self):
+        return self.students
