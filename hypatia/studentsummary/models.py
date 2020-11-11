f18e4e4f786f334899e99b954c580135b8b5d3f7
jennyziyi-xu <56279639+jennyziyi-xu@users.noreply.github.com>
2020-11-10 19:39:48 -0500

    Half way StudentManager class

--- hypatia/studentsummary/models.py
+++ hypatia/studentsummary/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class StudentManager(models.Model):
+    question_bank = models.ManyToManyField(Question)
+    students = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return self.students, self.questions
+    
+    def check_user(self, Student) -> bool:
+        """
+        Return whether the student is in the class. 
+        """
+        x = self.students.filter(name = Student.name)
+        if len(x) != 0:
+            return True
+        return False
+    
+    def get_all_questions(self):
+        return self.question_bank
+    
+    def get_questions_topic(self,Topic):
+        """
+        Return all questions for the topic. 
+        """
+        x = self.questions.filer(topic = Topic)
+        return x
+    
+    def append_question(self,Question):
+        
+
+
