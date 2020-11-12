c1de11257f0545792394acae6baf86a28a54a86c
ZhiYingSun <sophyzy.sun@mail.utoronto.ca>
2020-11-12 03:24:00 -0500

    fixed some typos in urls

--- hypatia/studentsummary/urls.py
+++ hypatia/studentsummary/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .questionapi import QuestionViewSet
 
 router = routers.DefaultRouter()
-routers.register('questionapi/question', QuestionViewSet, 'question') # (endpoint/question, viewset)
+router.register('questionapi/question', QuestionViewSet, 'question') # (endpoint/question, viewset)
 
-urlpatters = router.urls
+urlpatterns = router.urls
