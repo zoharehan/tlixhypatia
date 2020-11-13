e5242f3932774d82f4651a6b929f05760529934c
ZhiYingSun <sophyzy.sun@mail.utoronto.ca>
2020-11-13 17:16:10 -0500

    fixed some minor errors with admin.py

--- hypatia/studentsummary/urls.py
+++ hypatia/studentsummary/urls.py
@@ -3,8 +3,7 @@ from .questionapi import QuestionViewSet
 from .topicapi import TopicViewSet
 
 router = routers.DefaultRouter()
-router.register('questionapi/question', QuestionViewSet, 'question') # (endpoint/question, viewse
+router.register('questionapi/question', QuestionViewSet, 'question') # (endpoint/question, viewset)
 router.register('topicapi/topic',TopicViewSet,'topic')
 
 urlpatterns = router.urls
-                                    
