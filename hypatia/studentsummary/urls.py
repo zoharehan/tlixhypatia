e152cc41ab32c52f7c752c3ac0dc92e9c6592a97
jennyziyi-xu <56279639+jennyziyi-xu@users.noreply.github.com>
2020-11-13 16:20:04 -0500

    Topic router

--- hypatia/studentsummary/urls.py
+++ hypatia/studentsummary/urls.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 from .questionapi import QuestionViewSet
+from .topicapi import TopicViewSet
 
 router = routers.DefaultRouter()
 router.register('questionapi/question', QuestionViewSet, 'question') # (endpoint/question, viewse
+router.register('topicapi/topic',TopicViewSet,'topic')
 
 urlpatterns = router.urls
                                     
