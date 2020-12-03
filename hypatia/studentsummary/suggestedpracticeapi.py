8a5564e868000b070b243c0b339a43f5ba3d2ef3
ZhiYingSun <sophyzy.sun@mail.utoronto.ca>
2020-12-03 07:48:09 -0500

    added filtering for suggestedpractice api

--- hypatia/studentsummary/suggestedpracticeapi.py
+++ hypatia/studentsummary/suggestedpracticeapi.py
@@ -1,9 +1,15 @@
 from studentsummary.models import SuggestedPractice
 from rest_framework import viewsets, permissions
 from .serializers import SuggestedPracticeSerializer
+from django_filters import rest_framework as filters
+
 
 # SuggestedPractice Viewset
 # this lets us create a full CRUD API w/o having to specifiy specific methods for the functionality
+class SuggestedPracticeFilter(filters.FilterSet):
+    class Meta:
+        model = SuggestedPractice
+        fields = ['topic_most_missed']
 
 class SuggestedPracticeViewSet(viewsets.ModelViewSet):
     # a query that grabs all the Questions
@@ -13,3 +19,5 @@ class SuggestedPracticeViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = SuggestedPracticeSerializer
+    filter_class = SuggestedPracticeFilter
+    filterset_fields = ('topic_most_missed')
